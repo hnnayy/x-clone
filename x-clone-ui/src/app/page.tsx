f7850cbef76e25e71b354d183f3c10cf4c641b5f
hnnayy <mutiahanin2017@gmai.com>
2025-04-05 00:32:13 +0700

    upp

---
+++ x-clone-ui/src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Image from "next/image";
+import { IKImage } from "imagekitio-next";
+
+const urlEndpoint = "https://ik.imagekit.io/r88gseoed"; // Hanya endpoint, tanpa gambar
+
+export default function Home() {
+  return (
+    <div>
+      HomePage
+      <IKImage urlEndpoint={urlEndpoint} path="/general/default-image.jpg" />
+    </div>
+  );
+}
